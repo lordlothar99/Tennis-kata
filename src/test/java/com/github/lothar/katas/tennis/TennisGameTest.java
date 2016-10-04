a02ae97cfa43498dd4de2dcbc04b30309b0ab88a
François Lecomte <francois.lecomte@gmail.com>
2016-10-04 18:01:34 +0200

    should_both_players_have_0_points_when_game_starts ; test fails (compilation pb)

---
+++ src/test/java/com/github/lothar/katas/tennis/TennisGameTest.java
@@ -0,0 +1,17 @@
+package com.github.lothar.katas.tennis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class TennisGameTest {
+
+    @Test
+    public void should_both_players_have_0_points_when_game_starts() {
+        TennisGame tennisGame = new TennisGame();
+
+        assertThat(tennisGame.getPlayer1Score()).isEqualTo(0);
+        assertThat(tennisGame.getPlayer2Score()).isEqualTo(0);
+    }
+}
