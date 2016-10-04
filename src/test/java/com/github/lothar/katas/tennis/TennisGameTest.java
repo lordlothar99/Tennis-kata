935c2970e374057a0de0f3446502b43844504a79
François Lecomte <francois.lecomte@gmail.com>
2016-10-04 18:04:20 +0200

    should_score_be_15_0_when_first_player_wins_one_point ; test fails (compilation pb)

--- src/test/java/com/github/lothar/katas/tennis/TennisGameTest.java
+++ src/test/java/com/github/lothar/katas/tennis/TennisGameTest.java
@@ -13,4 +13,14 @@ public class TennisGameTest {
         assertThat(tennisGame.getPlayer1Score()).isEqualTo(0);
         assertThat(tennisGame.getPlayer2Score()).isEqualTo(0);
     }
+
+    @Test
+    public void should_score_be_15_0_when_first_player_wins_one_point() {
+        TennisGame tennisGame = new TennisGame();
+        
+        tennisGame.player1Scores();
+
+        assertThat(tennisGame.getPlayer1Score()).isEqualTo(15);
+        assertThat(tennisGame.getPlayer2Score()).isEqualTo(0);
+    }
 }
