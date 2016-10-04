b4e9c04bb4855fec5bf2dfc1a5ae3d9000865e20
François Lecomte <francois.lecomte@gmail.com>
2016-10-04 18:05:19 +0200

    should_score_be_15_0_when_first_player_wins_one_point ; test ok (dumb implementation)

--- src/main/java/com/github/lothar/katas/tennis/TennisGame.java
+++ src/main/java/com/github/lothar/katas/tennis/TennisGame.java
@@ -1,13 +1,19 @@
 package com.github.lothar.katas.tennis;
 
 public class TennisGame {
+    
+    private int player1Score = 0;
 
     public int getPlayer1Score() {
-        return 0;
+        return player1Score;
     }
 
     public int getPlayer2Score() {
         return 0;
+    }
+
+    public void player1Scores() {
+        player1Score = 15;
     }
 
 }
