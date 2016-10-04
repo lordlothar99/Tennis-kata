e39eb54ec80f47bd7555e936a212a76b9374dba9
François Lecomte <francois.lecomte@gmail.com>
2016-10-04 23:23:16 +0200

    should_board_display_winner_when_match_is_over ; test ok

--- src/test/java/com/github/lothar/katas/tennis/ResultBoardTest.java
+++ src/test/java/com/github/lothar/katas/tennis/ResultBoardTest.java
@@ -1,12 +1,6 @@
 package com.github.lothar.katas.tennis;
 
-import static com.github.lothar.katas.tennis.GameType.FIVE_SETS;
 import static com.github.lothar.katas.tennis.GameType.ONE_SET;
-import static com.github.lothar.katas.tennis.GameType.THREE_SETS;
-import static com.github.lothar.katas.tennis.Score.ADVANTAGE;
-import static com.github.lothar.katas.tennis.Score.FIFTEEN;
-import static com.github.lothar.katas.tennis.Score.FOURTY;
-import static com.github.lothar.katas.tennis.Score.THIRTY;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,13 +10,12 @@ public class ResultBoardTest {
 
     private Player player1 = new Player("John");
     private Player player2 = new Player("Bob");
-    private ScoreBoard scoreBoard = new ScoreBoard(asList(player1, player2), ONE_SET);
+    private ResultBoard resultBoard = new ResultBoard(asList(player1, player2), ONE_SET, player1);
 
     @Test
     public void should_board_display_winner_when_match_is_over() {
-        scoreBoard = new ResultBoard(asList(player1, player2), ONE_SET, player1);
         player1.setGamesWon(6);
-        assertThat(scoreBoard.toString()).isEqualTo("" + //
+        assertThat(resultBoard.toString()).isEqualTo("" + //
                 "| Player | Set 1 | Result |\n" + //
                 "| John   | 6     | WINNER |\n" + //
                 "| Bob    | 0     |        |\n");
