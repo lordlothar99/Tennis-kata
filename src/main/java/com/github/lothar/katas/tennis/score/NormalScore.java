23c539668660bc7ba5c09c24f34a4c956cb78f17
François Lecomte <francois.lecomte@gmail.com>
2016-10-11 00:24:41 +0200

    refactoring; NormalScore.next() uses ordinal

--- src/main/java/com/github/lothar/katas/tennis/score/NormalScore.java
+++ src/main/java/com/github/lothar/katas/tennis/score/NormalScore.java
@@ -1,36 +1,11 @@
 package com.github.lothar.katas.tennis.score;
 
 public enum NormalScore implements Score<NormalScore> {
-    ZERO("0") {
-        @Override
-        public NormalScore next() {
-            return FIFTEEN;
-        }
-    },
-    FIFTEEN("15") {
-        @Override
-        public NormalScore next() {
-            return THIRTY;
-        }
-    },
-    THIRTY("30") {
-        @Override
-        public NormalScore next() {
-            return FOURTY;
-        }
-    },
-    FOURTY("40") {
-        @Override
-        public NormalScore next() {
-            return ADVANTAGE;
-        }
-    },
-    ADVANTAGE("ADV") {
-        @Override
-        public NormalScore next() {
-            return null;
-        }
-    };
+    ZERO("0"), //
+    FIFTEEN("15"), //
+    THIRTY("30"), //
+    FOURTY("40"), //
+    ADVANTAGE("ADV");
 
     private String value;
 
@@ -41,5 +16,9 @@ public enum NormalScore implements Score<NormalScore> {
     @Override
     public String toString() {
         return value;
+    }
+
+    public NormalScore next() {
+        return NormalScore.values()[ordinal() + 1];
     }
 }
